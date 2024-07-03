7c4e169f9a7147fd7c74c30abc7568de03c12743
Your GitHub Username <davilama@unlv.nevada.edu>
2024-07-03 10:23:25 -0700

    Classwork finished

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,18 @@
+const { render, waitFor, screen } = require("@testing-library/react");
 
+import fetchMock from 'jest-fetch-mock'; fetchMock.enableMocks();
+import App from "./App";
+
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+  })
+
+// Resets to original state
+  beforeEach(() => {
+    fetch.resetMocks();
+  })
 
 
